5cac61fb397a74c92c7c2f667e6649d5973702f7
Tomii Poll <tommy@poolle.co.uk>
2021-09-26 20:27:45 +0100

    Add main loop

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from typing import List, Optional
 
 
 r = Robot()
+flag_motor = r.motor_board.motors[0]
+
+def main():
+    student_id = read_rfid()
+    if student_id is not None:
+        print(student_id)
 
 def read_rfid() -> Optional[str]:
-    result: Optional[str] = None
-
     try:
         response: List[str] = r.arduino._command("N")
         if len(response) != 1:
@@ -19,3 +23,7 @@ def read_rfid() -> Optional[str]:
     except CommunicationError as e:
         print(e)
         return None
+
+if __name__ == "__main__":
+    while True:
+        main()
